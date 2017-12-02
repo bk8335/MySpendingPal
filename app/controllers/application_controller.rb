80104ec0b8271951419d406ae8c103270043d402
bk8335 <bk8335@gmail.com>
2017-12-02 08:27:19 +0000

    google analytics

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def welcome
-  	render html: "myspendingpal or MySpendingPal ?"
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  root 'application#welcome'
+  get 'users/new'
+
+  get 'pages/home'
+
+  get '/about', to: 'pages#about'
+
+  get '/contact', to: 'pages#contact'
+
+  root 'pages#home'
 end

e88fbad45c53a6fa526d59a8bdd6fe080c67dfff
bk8335 <bk8335@gmail.com>
2018-01-31 20:59:09 +0000

    month saving before save

--- config/routes.rb
+++ config/routes.rb
@@ -58,4 +58,7 @@ get    '/login',   to: 'sessions#new'
 post   '/login',   to: 'sessions#create'
 delete '/logout',  to: 'sessions#destroy'
 
+#account activation
+resources :account_activations, only: [:edit]
+
 end

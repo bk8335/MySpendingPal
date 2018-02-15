fd96b2c0cd3a681e67d11072e01a87c2748dcbd4
bk8335 <bk8335@gmail.com>
2018-02-15 20:10:24 +0000

    rails tutorial ch11 emails and homepage revamp

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
 	attr_accessor :remember_token, :activation_token
-	before_save { self.email = email.downcase }
+	before_save :downcase_email
   before_create :create_activation_digest
 	validates :name, presence: true, length: { maximum: 255 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -26,9 +26,10 @@ class User < ApplicationRecord
   end
 
   # Returns true if the given token matches the digest.
-  def authenticated?(remember_token)
-  	return false if remember_digest.nil?
-  	BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+  	return false if digest.nil?
+  	BCrypt::Password.new(digest).is_password?(token)
   end
 
   # Forgets a user.
@@ -41,6 +42,16 @@ class User < ApplicationRecord
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  # Sends activation email.
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
   end
 
   def disposable_income(user)
@@ -252,11 +263,15 @@ class User < ApplicationRecord
     ((number / daily_spending_total(user))*100).round(1)
   end
 
-  private
+private
 
-  # Creates and assigns the activation token and digest.
-    def create_activation_digest
-      self.activation_token  = User.new_token
-      self.activation_digest = User.digest(activation_token)
+# Creates and assigns the activation token and digest.
+  def create_activation_digest
+    self.activation_token  = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
+  def downcase_email
+      self.email = email.downcase
     end
 end
